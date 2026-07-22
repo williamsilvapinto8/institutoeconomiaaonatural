f6049cac7a3e92a780ce8d7692cb497f2f139e8c
William Silva Pinto <william.silvapinto@gmail.com>
2026-07-22 02:48:28 -0300

    feat: initialize Django project settings with custom configuration, database connection, and production security policies

--- setup/settings.py
+++ setup/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # causado pelo caractere 'ç' no path do Windows com a biblioteca libpq do PostgreSQL.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT', default='5432'),
     }
 }
 
